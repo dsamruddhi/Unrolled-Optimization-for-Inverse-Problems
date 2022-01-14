7d40f6b246943f9dbfa322d86d7dfaf081607912
Samruddhi Deshmukh <dsamruddhi21@gmail.com>
2022-01-14 14:47:51 +0800

    added ADMM L1 layer

--- layers/optimization_layers.py
+++ layers/optimization_layers.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 
+from prox_operators import ProxOperators
+
 
 class ProximalL1Layer(layers.Layer):
     """
@@ -14,16 +16,29 @@ class ProximalL1Layer(layers.Layer):
         self.A = tf.Variable(initial_value=A, dtype=tf.float32, trainable=False)
         self.eta = tf.Variable(initial_value=eta, dtype=tf.float32, trainable=True)
 
-    @staticmethod
-    def soft_threshold(x, threshold, name=None):
-        with tf.name_scope(name or 'soft_threshold'):
-            x = tf.convert_to_tensor(x, name='x')
-            threshold = tf.convert_to_tensor(threshold, dtype=x.dtype, name='threshold')
-            return tf.sign(x) * tf.maximum(tf.abs(x) - threshold, 0.)
-
     def call(self, inputs, **kwargs):
         [xt, y] = inputs
         nabla_f = tf.matmul(tf.transpose(self.A), tf.matmul(self.A, xt) - y[0])
         zt1 = xt - (self.eta * nabla_f)
-        xt1 = ProximalL1Layer.soft_threshold(zt1, self.eta)
+        xt1 = ProxOperators.soft_threshold(zt1, self.eta)
         return [xt1, y]
+
+
+class ADMML1Layer(layers.Layer):
+    """
+    Single layer for an unrolled optimization network performing least squares minimization with l1 constraints
+    solved using the Alternating Direction Method of Multipliers Algorithm.
+    """
+
+    def __init__(self, A, eta):
+
+        super(ADMML1Layer, self).__init__()
+        self.A = tf.Variable(initial_value=A, dtype=tf.float32, trainable=False)
+        self.eta = tf.Variable(initial_value=eta, dtype=tf.float32, trainable=True)
+
+    def call(self, inputs, **kwargs):
+        [xt, st, y] = inputs
+        zt1 = ProxOperators.least_squares_prox(self.A, xt - st, y, self.eta)
+        xt1 = ProxOperators.soft_threshold(zt1 + st, self.eta)
+        st1 = st + (zt1 - xt1)
+        return [xt1, st1, y]
--- layers/prox_operators.py
+++ layers/prox_operators.py
@@ -12,13 +12,13 @@ class ProxOperators:
             return tf.sign(x) * tf.maximum(tf.abs(x) - threshold, 0.)
 
     @staticmethod
-    def least_squares_prox(A, x, y, gamma, name=None):
-        """ Proximal operator for the term ||y - Ax||^2_2 = (I + gamma A'A)^-1 * (x + gamma A'y) """
+    def least_squares_prox(A, x, y, eta, name=None):
+        """ Proximal operator for the term ||y - Ax||^2_2 = (I + eta A'A)^-1 * (x + eta A'y) """
         with tf.name_scope(name or 'least_squares_prox'):
             A = tf.convert_to_tensor(A, name='A')
             x = tf.convert_to_tensor(x, name='x')
             y = tf.convert_to_tensor(y, name='y')
-            gamma = tf.convert_to_tensor(gamma, name='gamma')
+            gamma = tf.convert_to_tensor(eta, name='gamma')
             t1 = tf.linalg.inv(tf.eye(A.shape[1]) + gamma * tf.multiply(tf.transpose(A), A))
             t2 = x + gamma * tf.multiply(tf.transpose(A), y)
             return tf.multiply(t1, t2)
